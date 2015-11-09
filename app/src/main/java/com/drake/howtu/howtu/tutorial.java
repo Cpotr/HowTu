3ce8f501ccc5cbf7fb09ae2007370e6cf6135552
Connor Finholt <connor.finholt@gmail.com>
2015-11-09 16:47:07 -0600

    Finally made it so that the gif is resized for the window of the given device.

--- app/src/main/java/com/drake/howtu/howtu/tutorial.java
+++ app/src/main/java/com/drake/howtu/howtu/tutorial.java
@@ -7,6 +7,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class tutorial extends AppCompatActivity {
 
@@ -15,8 +16,12 @@ public class tutorial extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GifWebView view = new GifWebView(this, "file:///android_asset/how_tu_gif_t.gif");
+        GifWebView view = new GifWebView(this, "file:///android_asset/gif_formatter.html");
         setContentView(view);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         timer = new CountDownTimer(25000, 1000) {
             @Override
